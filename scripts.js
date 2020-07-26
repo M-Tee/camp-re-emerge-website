c12a62b426b5a75756ad2075da526e8207247faa
mt <margarettom6@gmail.com>
2020-07-26 09:47:25 +0300

    Home content text animation

--- scripts.js
+++ scripts.js
@@ -17,24 +17,38 @@ anime.timeline({loop: false})
     delay: 1000
   });
 
-// let homeContent = document.querySelector('.ml12');
-// homeContent.innerHTML = homeContent.textContent.replace(/\S/g, "<span class='letters'>$&</span>");
+let subtitle = document.querySelector('.sub-title');
+subtitle.innerHTML = subtitle.textContent.replace(/\S/g, "<span class='letters'>$&</span>");
 
-// anime.timeline({loop: false})
-//   .add({
-//     targets: '.ml12 .letters',
-//     translateX: [40,0],
-//     translateZ: 0,
-//     opacity: [0,1],
-//     easing: "easeOutExpo",
-//     duration: 1200,
-//     delay: (el, i) => 1500 + 30 * i
-//   })
-//   // .add({
-//   //   targets: '.ml12 .letter',
-//   //   translateX: [0, 10],
-//   //   easing: "easeInExpo",
-//   //   duration: 1100,
-//   //   delay: (el, i) => 100 + 30 * i
-//   // })
-//   ;
+anime.timeline({loop: false})
+  .add({
+    targets: '.sub-title .letters',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 5000,
+    delay: (el, i) => 1500 + 30 * i
+  });
+
+let homeContent = document.querySelector('.ml12');
+homeContent.innerHTML = homeContent.textContent.replace(/\S/g, "<span class='letters'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml12 .letters',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 5000,
+    delay: (el, i) => 2500 + 30 * i
+  });
+  // .add({
+  //   targets: '.ml12 .letter',
+  //   translateX: [0, 10],
+  //   easing: "easeInExpo",
+  //   duration: 1100,
+  //   delay: (el, i) => 100 + 30 * i
+  // })
+  
